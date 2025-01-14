a8e980e649a4e9451d7559741df0c4a7e533729e
nakamura-tomoya22135 <cito22135@g.nihon-u.ac.jp>
2025-01-14 11:59:59 +0900

    13-14

--- codes/com/craftinginterpreters/lox/AstPrinter.java
+++ codes/com/craftinginterpreters/lox/AstPrinter.java
@@ -26,6 +26,23 @@ class AstPrinter implements Expr.Visitor<String> {
     return parenthesize(expr.operator.lexeme, expr.right);
   }
 
+  @Override
+  public String visitVariableExpr(Expr.Variable expr) {
+    return "";
+  }
+
+  @Override
+  public String visitAssignExpr(Expr.Assign expr) {
+    return "";
+  }
+  
+  @Override
+  public String visitLogicalExpr(Expr.Logical expr) {
+    return "";
+  }
+
+
+
   private String parenthesize(String name, Expr... exprs) {
     StringBuilder builder = new StringBuilder();
 
--- codes/com/craftinginterpreters/lox/Interpreter.java
+++ codes/com/craftinginterpreters/lox/Interpreter.java
@@ -11,6 +11,9 @@ class Interpreter implements Expr.Visitor<Object>,
     //      Lox.runtimeError(error);
     //    }
     //}
+
+    private Environment environment = new Environment();
+
     void interpret(List<Stmt> statements) {
         try {
             for (Stmt statement : statements) {
@@ -27,6 +30,19 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+        if (isTruthy(left)) return left;
+        } else {
+        if (!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
+    }
+
+    @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
         switch (expr.operator.type) {
@@ -38,6 +54,11 @@ class Interpreter implements Expr.Visitor<Object>,
 
         // Unreachable.
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
@@ -90,9 +111,38 @@ class Interpreter implements Expr.Visitor<Object>,
         stmt.accept(this);
     }
 
+    void executeBlock(List<Stmt> statements,Environment environment) {
+        Environment previous = this.environment;
+        try {
+        this.environment = environment;
+
+        for (Stmt statement : statements) {
+            execute(statement);
+        }
+        } finally {
+        this.environment = previous;
+        }
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
         evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(evaluate(stmt.condition))) {
+        execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+        execute(stmt.elseBranch);
+        }
         return null;
     }
 
@@ -102,6 +152,33 @@ class Interpreter implements Expr.Visitor<Object>,
         System.out.println(stringify(value));
         return null;
     }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+        execute(stmt.body);
+        }
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+
 
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
